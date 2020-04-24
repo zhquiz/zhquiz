0304329635ab4b0e332821a2269e866fe349b03e
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-04-25 00:47:55 +0700

    add quiz

---
+++ packages/server/scripts/add-hanzi-def.ts
@@ -0,0 +1,24 @@
+import fs from 'fs'
+
+import csv from 'neat-csv'
+import sqlite3 from 'better-sqlite3'
+
+async function main () {
+  const db = sqlite3('assets/zh.db')
+  const stmt = db.prepare(/*sql*/`
+  UPDATE token
+  SET pinyin = ?, english = ?
+  WHERE [entry] = ?
+  `)
+
+  const data = await csv(fs.createReadStream('/Users/patarapolw/Downloads/CharFreq-Combined.csv', 'utf8'), {
+    headers: ['id', 'hanzi', 'count', 'percentile', 'pinyin', 'english']
+  })
+  data.map((d) => {
+    stmt.run(d.pinyin || null, d.english || null, d.hanzi)
+  })
+
+  db.close()
+}
+
+main()
