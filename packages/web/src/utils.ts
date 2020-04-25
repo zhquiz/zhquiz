c1212b9b7094b123fae98e36f40ca003a6da41c7
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-04-25 17:42:24 +0700

    add quiz editing

--- packages/web/src/utils.ts
+++ packages/web/src/utils.ts
@@ -63,3 +63,88 @@ export function speak (s: string, lang: string = 'zh') {
     speechSynthesis.speak(utterance)
   }
 }
+
+export function humanizeDuration (duration: number) {
+  if (!duration || typeof duration !== 'number' || duration < 0) {
+    return ''
+  }
+
+  /**
+   * @type {string[]}
+   */
+  const stack = []
+
+  /**
+   * ms
+   */
+  let div = divideAndRemainder(duration, 1000)
+  duration = div.result
+
+  /**
+   * s
+   */
+  div = divideAndRemainder(duration, 60)
+  duration = div.result
+  stack.push(div.remainder ? `${div.remainder}s` : null)
+
+  /**
+   * min
+   */
+  div = divideAndRemainder(duration, 60)
+  duration = div.result
+  stack.push(div.remainder ? `${div.remainder}m` : null)
+
+  /**
+   * h
+   */
+  div = divideAndRemainder(duration, 24)
+  duration = div.result
+  stack.push(div.remainder ? `${div.remainder}h` : null)
+
+  /**
+   * d
+   */
+  div = divideAndRemainder(duration, 7)
+  stack.push(div.remainder ? `${div.remainder}d` : null)
+
+  /**
+   * w
+   */
+  const w = div.result % 4
+  stack.push(w ? `${w}w` : null)
+
+  /**
+   * mo
+   */
+  const mo = Math.floor(duration / 30) % 12
+  stack.push(mo ? `${mo}mo` : null)
+
+  /**
+   * y
+   */
+  const y = Math.floor(duration / 365)
+  stack.push(y ? `${y}y` : null)
+
+  let j: number | null = null
+
+  return stack
+    .reverse()
+    .filter((s, i) => {
+      if (j === null && s !== null) {
+        j = i
+        return true
+      }
+      if (j !== null && i < j + 2) {
+        return true
+      }
+      return false
+    })
+    .join(' ')
+}
+
+function divideAndRemainder (n: number, by: number) {
+  return {
+    result: Math.floor(n / by),
+    remainder: n % by
+  }
+}
