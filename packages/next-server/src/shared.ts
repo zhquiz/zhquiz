457f6c91275107ebdfe7214a56f29e103f4cb102
Pacharapol Withayasakpunt <polv@polv.cc>
2021-01-06 02:09:16 +0700

    remove vulnerable packages

--- packages/next-server/src/shared.ts
+++
@@ -1,3 +0,0 @@
-import { Serialize } from 'any-serialize'
-
-export const ser = new Serialize()
--- packages/nuxt/plugins/firebase-auth.client.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from '@nuxt/types'
-
-const onInit: Plugin = ({ app }) => {
-  app.$fireAuth.onAuthStateChanged((user) => {
-    app.$accessor.updateUser(user)
-  })
-}
-
-export default onInit
