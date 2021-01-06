acbc915483d13ca0f239c146979f5349322dc91c
Pacharapol Withayasakpunt <polv@polv.cc>
2021-01-06 23:14:26 +0700

    update build script and README

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ ZhQuiz, a Chinese quizzing platform
 - HSK vocabularies made into 60 levels
 - Flashcards showing statuses of success
 - Custom vocabularies input by users
+
+## Desktop app
+
+See <https://github.com/zhquiz/zhquiz/releases>.
+
+For Windows, you will also need to install Chinese Language Support.
+
+For macOS, you will need to enable Chinese voice (Ting-Ting) in accessibility.
+
+For Linux, you might need to install `espeak` and `speech-dispatcher`, if not preinstalled already.
--- packages/nodejs/scripts/dist.ts
+++ packages/nodejs/scripts/dist.ts
@@ -1,24 +1,64 @@
 import AdmZip from 'adm-zip'
-import fs from 'fs'
+import { execSync } from 'child_process'
+import fs from 'fs-extra'
 import glob from 'fast-glob'
 
 process.chdir('../../submodules/server')
 
-glob.sync('./zhquiz-darwin-*').map((f) => {
-  if (!f.endsWith('.app')) {
-    fs.renameSync(f, f + '.app')
-  }
-})
+execSync('rm -rf ../../dist')
+fs.ensureDirSync('../../dist')
 
 glob.sync('./zhquiz-*').map((f) => {
-  const zip = new AdmZip()
-  zip.addLocalFolder('./assets', 'assets')
-  zip.addLocalFolder('./public', 'public')
-  zip.addLocalFile(`./${f}`)
+  if (/-darwin-/.test(f)) {
+    fs.ensureDirSync(`../../dist/${f}.app/Contents/MacOS`)
 
-  if (!fs.existsSync('../../dist')) {
-    fs.mkdirSync('../../dist')
+    fs.writeFileSync(
+      `../../dist/${f}.app/Contents/Info.plist`,
+      `
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>CFBundleExecutable</key>
+  <string>${f}</string>
+  <key>CFBundleIconFile</key>
+  <string>favicon.icns</string>
+  <key>CFBundleIdentifier</key>
+  <string>cc.zhquiz.${f}</string>
+
+  <!-- avoid having a blurry icon and text -->
+  <key>NSHighResolutionCapable</key>
+  <string>True</string>
+
+  <!-- avoid showing the app on the Dock -->
+  <key>LSUIElement</key>
+  <string>1</string>
+</dict>
+</plist>
+    `.trim()
+    )
+
+    fs.ensureDirSync(`../../dist/${f}.app/Contents/Resources`)
+
+    // sudo apt-get install icnsutils
+    execSync(
+      `png2icns ../../dist/${f}.app/Contents/Resources/favicon.icns public/icon.png`
+    )
+
+    fs.copySync('./assets', `../../dist/${f}.app/Contents/MacOS/assets`)
+    fs.copySync('./public', `../../dist/${f}.app/Contents/MacOS/public`)
+    fs.copyFileSync(f, `../../dist/${f}.app/Contents/MacOS/${f}`)
+
+    const zip = new AdmZip()
+    zip.addLocalFolder(`../../dist/${f}.app`, `${f}.app`)
+
+    zip.writeZip(`../../dist/${f}.zip`)
+  } else {
+    const zip = new AdmZip()
+    zip.addLocalFolder('./assets', 'assets')
+    zip.addLocalFolder('./public', 'public')
+    zip.addLocalFile(`./${f}`)
+
+    zip.writeZip(`../../dist/${f.replace(/\.[^.-]+$/, '')}.zip`)
   }
-
-  zip.writeZip(`../../dist/${f.replace(/\.[^.-]+$/, '')}.zip`)
 })
