20050d9f5256db1321dfcd43ffb5a1738b6fa81e
Pacharapol Withayasakpunt <polv@polv.cc>
2021-01-08 00:28:32 +0700

    more db fix and testing

--- packages/nodejs/src/vocab/add-update.ts
+++ packages/nodejs/src/vocab/add-update.ts
@@ -7,41 +7,41 @@ import yaml from 'js-yaml'
 async function main() {
   const db = sqlite('../../submodules/server/assets/zh.db')
 
-  const zVocab = z.record(
-    z.object({
-      _meta: z.object({
-        source: z.string()
-      }),
-      vocab: z.array(z.string())
-    })
-  )
+  // const zVocab = z.record(
+  //   z.object({
+  //     _meta: z.object({
+  //       source: z.string()
+  //     }),
+  //     vocab: z.array(z.string())
+  //   })
+  // )
 
-  const stmt = db.prepare(/* sql */ `
-  UPDATE token
-  SET tag = @tag
-  WHERE [entry] = @entry
-  `)
-
-  db.transaction(() => {
-    Object.values(
-      zVocab.parse(
-        yaml.safeLoad(fs.readFileSync('../../assets/vocab.yaml', 'utf-8'))
-      )
-    ).map(({ _meta: { source }, vocab }) => {
-      vocab.map((v) => {
-        stmt.run({
-          tag: source,
-          entry: v
-        })
-      })
-    })
-  })()
-
-  // db.exec(/* sql */ `
-  // UPDATE vocab
-  // SET frequency = NULL
-  // WHERE english LIKE 'surname %'
+  // const stmt = db.prepare(/* sql */ `
+  // UPDATE token
+  // SET tag = @tag
+  // WHERE [entry] = @entry
   // `)
+
+  // db.transaction(() => {
+  //   Object.values(
+  //     zVocab.parse(
+  //       yaml.safeLoad(fs.readFileSync('../../assets/vocab.yaml', 'utf-8'))
+  //     )
+  //   ).map(({ _meta: { source }, vocab }) => {
+  //     vocab.map((v) => {
+  //       stmt.run({
+  //         tag: source,
+  //         entry: v
+  //       })
+  //     })
+  //   })
+  // })()
+
+  db.exec(/* sql */ `
+  UPDATE vocab
+  SET frequency = NULL
+  WHERE english LIKE 'abbr. for %'
+  `)
 
   // console.log(
   //   db
