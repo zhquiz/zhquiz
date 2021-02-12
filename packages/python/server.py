3ecea46d452fc79e6e079e22ba53eb9015c29de7
patarapolw <polv@polv.cc>
2021-02-13 06:27:31 +0700

    update python server

--- packages/python/server.py
+++ packages/python/server.py
@@ -1,15 +1,41 @@
 from typing import List
 from wordfreq import word_frequency
-
+from regex import regex
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+def wordfreq(it: str, lang: str):
+    if lang == "zh" and regex.search("[\\p{Katakana}\\p{Hiragana}]", it):
+        return 0
+
+    return word_frequency(it, lang) * 10 ** 6
+
+
 @app.post("/wordfreq")
-async def read_root(entries: List[str]):
+async def r_wordfreq(entries: List[str], lang: str = "zh"):
     out = {}
     for it in entries:
-        out[it] = word_frequency(it, "zh") * 10 ** 6
+        out[it] = wordfreq(it, lang)
 
     return out
+
+
+@app.post("/wordfreq/multi")
+async def r_wordfreq_multi(entries: List[str], langs: List[str]):
+    out = []
+    for it in entries:
+        r = {}
+
+        for lang in langs:
+            r[lang] = wordfreq(it, lang)
+
+            if not r[lang]:
+                r[lang] = None
+
+        if any(r.values()):
+            r["entry"] = it
+            out.append(r)
+
+    return {"result": out}
