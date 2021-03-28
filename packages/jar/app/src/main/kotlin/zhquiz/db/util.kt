78dd14439efb0935dab2321f5cee2a0aad5bacb8
patarapolw <polv@polv.cc>
2021-03-29 01:21:12 +0700

    remove kotlin code

--- packages/jar/app/src/main/kotlin/zhquiz/db/util.kt
+++
@@ -1,9 +0,0 @@
-package zhquiz.db
-
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import java.security.SecureRandom
-import java.util.*
-
-val gson: Gson = GsonBuilder()
-    .create()
