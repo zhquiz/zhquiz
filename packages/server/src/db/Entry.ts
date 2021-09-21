24587e11e7ef2aa88cd98657d72d1fd268095a81
Pacharapol Withayasakpunt <polv@polv.cc>
2021-09-21 15:30:02 +0700

    add reading, meaning, translation generators

--- packages/server/src/db/Entry.ts
+++ packages/server/src/db/Entry.ts
@@ -1,6 +1,33 @@
-import { getModelForClass, index, prop } from '@typegoose/typegoose'
+import { getModelForClass, index, pre, prop } from '@typegoose/typegoose'
+import makePinyin from 'chinese-to-pinyin'
+import S from 'jsonschema-definer'
+import jieba from 'nodejieba'
 import shortUUID from 'short-uuid'
 
+@pre<DbEntry>('validate', async function (next) {
+    const entry = S.string().ensure(this.entry[0]!)
+
+    let segments: any[] = []
+
+    if (!this.translation || !this.translation.length) {
+        segments = await this.makeTranslation()
+    }
+
+    if (!this.level && this.type === 'sentence') {
+        this.level = await this.makeLevel(segments)
+    }
+
+    if (!this.reading || !this.reading.length) {
+        this.reading = [
+            makePinyin(entry, {
+                toneToNumber: true,
+                keepRest: true,
+            }),
+        ]
+    }
+
+    next()
+})
 @index({ translation: 'text', description: 'text' })
 class DbEntry {
     @prop({ default: () => shortUUID.generate() }) _id!: string
@@ -16,13 +43,11 @@ class DbEntry {
     @prop({
         required: true,
         type: [String],
-        validate: (v: string[]) => !!v[0],
         index: true,
     })
     entry!: string[]
 
     @prop({
-        default: () => [],
         type: [String],
         index: true,
     })
@@ -31,6 +56,7 @@ class DbEntry {
     @prop({
         default: () => [],
         type: [String],
+        index: true,
     })
     translation!: string[]
 
@@ -45,6 +71,141 @@ class DbEntry {
         index: true,
     })
     tag!: string[]
+
+    get simplified() {
+        return this.entry[0]!
+    }
+
+    get alt() {
+        return this.entry.slice(1)
+    }
+
+    makeReading(): string {
+        return makePinyin(this.simplified, {
+            toneToNumber: true,
+            keepRest: true,
+        })
+    }
+
+    async makeTranslation(): Promise<
+        {
+            entry: string[]
+            reading: string[]
+            translation: string[]
+            level?: number
+        }[]
+    > {
+        const segments = await DbEntryModel.find(
+            {
+                entry: {
+                    $in: [
+                        ...new Set(jieba.cutForSearch(this.simplified)),
+                    ].filter((v) => /\p{sc=Han}/u.test(v)),
+                },
+                type: 'vocabulary',
+            },
+            { entry: 1, reading: 1, translation: 1, level: 1 }
+        )
+
+        const entriesMap = new Map<
+            string,
+            {
+                entry: string[]
+                reading: string[]
+                translation: string[]
+            }
+        >()
+        segments.map((et) => {
+            const et0 = et.entry[0]!
+            let it = entriesMap.get(et0)
+            if (!it) {
+                it = {
+                    entry: et.entry,
+                    reading: et.reading,
+                    translation: et.translation,
+                }
+            } else {
+                const alt = [
+                    ...new Set([...it.entry.slice(1), ...et.entry.slice(1)]),
+                ].sort()
+                it.entry = [et0, ...alt]
+
+                const rUpper: string[] = []
+                const rLower: string[] = []
+
+                ;[...new Set([...it.reading, ...et.reading])]
+                    .sort()
+                    .map((r) => {
+                        if (r[0] && r[0].toLocaleLowerCase() === r[0]) {
+                            rLower.push(r)
+                        } else {
+                            rUpper.push(r)
+                        }
+                    })
+
+                it.reading = [...rLower, ...rUpper]
+
+                it.translation = [
+                    ...new Set([...it.translation, ...et.translation]),
+                ].sort()
+            }
+            entriesMap.set(et0, it)
+        })
+
+        this.translation = [
+            [...entriesMap]
+                .map(
+                    ([k, v]) =>
+                        `- ${v.entry.join(' ')} [${
+                            v.reading.length
+                                ? v.reading.join(' / ')
+                                : makePinyin(k, {
+                                      toneToNumber: true,
+                                      keepRest: true,
+                                  })
+                        }] ${v.translation.join(' / ')}`
+                )
+                .join('\n'),
+        ]
+
+        return segments
+    }
+
+    async makeLevel(
+        segments: {
+            entry: string[]
+            level?: number
+        }[] = []
+    ) {
+        const raw = [...new Set(jieba.cutForSearch(this.simplified))].filter(
+            (v) => /\p{sc=Han}/u.test(v)
+        )
+
+        if (!segments.length) {
+            segments = await DbEntryModel.find(
+                {
+                    entry: { $in: raw },
+                    type: 'vocabulary',
+                    level: { $exists: true },
+                },
+                { entry: 1, level: 1 }
+            )
+        }
+
+        const entriesMap = new Map<string, number>()
+        segments.map((et) => {
+            if (et.level) {
+                entriesMap.set(S.string().ensure(et.entry[0]!), et.level)
+            }
+        })
+
+        this.level =
+            ([...entriesMap.values()].reduce((prev, c) => prev + c, 0) *
+                raw.length) /
+            entriesMap.size
+
+        return this.level
+    }
 }
 
 export const DbEntryModel = getModelForClass(DbEntry, {
